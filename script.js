a6e1af5ea47f80ad30d92239edb42eed5031ff2c
Umar <umaralfaaz14@gmail.com>
2025-05-20 18:20:46 +0300

    Second push

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const btn = document.getElementById('clickBtn');
 const msg = document.getElementById('message');
 
 btn.addEventListener('click', () => {
-  msg.textContent = 'Awesome! You clicked the button and updated the feature branch.';
+  msg.textContent = 'yuhooo! You clicked the button and updated the feature branch.';
 });
